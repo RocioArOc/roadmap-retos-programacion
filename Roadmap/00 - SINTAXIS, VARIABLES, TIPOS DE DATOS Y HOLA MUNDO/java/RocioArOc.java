406a57f5d2d76758d2296ff7459554ec1f810479
Rocio Arias Ocón <rao24gr034@student.esgerencia.com>
2025-06-24 11:44:05 +0200

    #00 - Java

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/RocioArOc.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+
+        /* Lenguaje elijo: Java
+        https://www.java.com/es/ */
+        // Esto es un comentario de una línea
+        /* Esto es un
+        comentario
+        de varias líneas */
+
+        //Creación de una variable y una constante
+        String variable = "Esto es una variable tipo string";
+        final String constante = "Esto es una constante";
+
+        //Tipos de datos
+        int num1 = 28;
+        long num2 = 73645;
+        float decimal = 26.9f;
+        double decimal2 = 75.6;
+        String cadena ="Esto es una cadena de texto";
+        char letra ='a';
+        boolean boleano = true;
+
+        System.out.println("¡Hola, Java!");
+
+    }
+}
